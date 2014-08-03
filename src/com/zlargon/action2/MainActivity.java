15ad79ebf4c5560b8b5e2198e270c3b79cc63e6e
leon_huang <leon_huang@gemteks.com>
2014-08-03 12:20:04 +0800

    [12:20] add prev button

--- src/com/zlargon/action2/MainActivity.java
+++ src/com/zlargon/action2/MainActivity.java
@@ -5,28 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener {
-
-    Button button1;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // get button1
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        // show detail activity
+    public void gotoNext(View v) {
+        // goto detail activity
         Intent intent = new Intent(this, DetailActivity.class);
         startActivity(intent);
+    }
+
+    public void gotoPrev(View v) {
+        finish();
     }
 
     @Override
