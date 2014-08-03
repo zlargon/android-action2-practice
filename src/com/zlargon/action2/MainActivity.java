91be4617c65749545488ad10f7f5011e65c9dfd3
leon_huang <leon_huang@gemteks.com>
2014-08-03 11:42:19 +0800

    [11:42] p.218-221

--- src/com/zlargon/action2/MainActivity.java
+++ src/com/zlargon/action2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.zlargon.action2;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get button1
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
