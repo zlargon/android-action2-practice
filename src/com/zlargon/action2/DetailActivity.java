afd12c8c59f7ce027067b9f8efb0cd2b1d6151d4
leon_huang <leon_huang@gemteks.com>
2014-08-03 12:08:19 +0800

    [12:08] clear top and no clear top

--- src/com/zlargon/action2/DetailActivity.java
+++ src/com/zlargon/action2/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.zlargon.action2;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class DetailActivity extends Activity {
 
@@ -9,5 +13,31 @@ public class DetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // No Clear Top
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        // intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Clear Top
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
